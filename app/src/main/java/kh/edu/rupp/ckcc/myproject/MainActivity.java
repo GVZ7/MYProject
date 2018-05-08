dbfb460b115608a27ddf8b36a1ae8744a5d97333
Sok Im Koeu <koeusokim@gmail.com>
2018-05-08 19:25:57 +0700

    Initial commit

---
+++ app/src/main/java/kh/edu/rupp/ckcc/myproject/MainActivity.java
@@ -0,0 +1,13 @@
+package kh.edu.rupp.ckcc.myproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
