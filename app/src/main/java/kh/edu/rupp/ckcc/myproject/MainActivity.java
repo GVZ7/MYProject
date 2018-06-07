09aa5ff0e2800829025e59751235c6747e5aac4c
Y <vinsovann6969@gmail.com>
2018-06-07 19:38:42 +0700

    fresco

--- app/src/main/java/kh/edu/rupp/ckcc/myproject/MainActivity.java
+++ app/src/main/java/kh/edu/rupp/ckcc/myproject/MainActivity.java
@@ -1,5 +1,6 @@
 package kh.edu.rupp.ckcc.myproject;
 
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -8,23 +9,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import org.w3c.dom.Text;
+
+import javax.annotation.Nullable;
+
 public class MainActivity extends AppCompatActivity {
  DrawerLayout drawerLayout;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fresco.initialize(this);
         setContentView(R.layout.activity_homepage);
+
         drawerLayout = findViewById(R.id.lyt_main);
-        NavigationView navigationView = findViewById(R.id.navigation_view);
+         navigationView = findViewById(R.id.navigation_view);
        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
            @Override
            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -50,35 +65,35 @@ public class MainActivity extends AppCompatActivity {
            }
 
        });
-
+        loaddata();
     }
     // loading username Email in drawer
-   private void loaddata()
-   {
-      
+   private void loaddata() {
+
        FirebaseFirestore db = FirebaseFirestore.getInstance();
        //read data
-       db.collection("Profile").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+       db.collection("Profile").document("ndqLK4kQlgD2osE16iOF").addSnapshotListener(new EventListener<DocumentSnapshot>() {
            @Override
-           //catch all data put into task
-           public void onComplete(@NonNull Task<QuerySnapshot> task) {
-               //check task error
-               if (task.isSuccessful())
-               {
-                   QuerySnapshot document=task.getResult();
+           public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                    if(documentSnapshot==null){
+                        Toast.makeText(getApplication(),"Error",Toast.LENGTH_LONG).show();
+                        Log.d("Ckcc","LoadDataError: "+e);
+                    }
+                    else{
+                        Profile profile = documentSnapshot.toObject(Profile.class);
+                        View headerView= navigationView.getHeaderView(0);
+                        TextView txtName = headerView.findViewById(R.id.username);
+                        txtName.setText(profile.getUsername());
+                        TextView txtEmail= headerView.findViewById(R.id.email);
+                        txtEmail.setText(profile.getEmail());
 
-
-               }else
-               {
-                   Toast.makeText(getApplication(),"Error Loading Prfile",Toast.LENGTH_SHORT).show();
-                   Log.d("MY App","Error Loading"+task.getException().getMessage());
-               }
-
+                        //imgURL
+                        SimpleDraweeView imgUrl =headerView.findViewById(R.id.img_profile);
+                        imgUrl.setImageURI(profile.getImgUrl());
+                    }
            }
        });
-
    }
-
    //create class Profile
 //    public class Profile
 //   {
