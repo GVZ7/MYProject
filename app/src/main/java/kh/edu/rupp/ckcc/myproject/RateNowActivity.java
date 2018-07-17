b80db919516752b032089f537b47a1e411c9b2e0
vourchleang <cvourchleang@gmail.com>
2018-07-17 12:18:31 -0400

    [feedback with email]

---
+++ app/src/main/java/kh/edu/rupp/ckcc/myproject/RateNowActivity.java
@@ -0,0 +1,40 @@
+package kh.edu.rupp.ckcc.myproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class RateNowActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button feedback,rate_now;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_now);
+        feedback = findViewById(R.id.feedback);
+        rate_now = findViewById(R.id.btnRateNow);
+
+        feedback.setOnClickListener(this);
+        rate_now.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == feedback){
+            //Start MainActivity
+            Intent intent = new Intent(this,feeback_activity.class);
+            startActivity(intent);
+            //Finish current activity
+            finish();
+        }else if(v == rate_now){
+            Intent intent = new Intent(this,RateNow.class);
+            startActivity(intent);
+            finish();
+        }else {
+            Toast.makeText(this, "Please Other Click Button", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- app/src/main/java/kh/edu/rupp/ckcc/myproject/feeback_activity.java
+++ app/src/main/java/kh/edu/rupp/ckcc/myproject/feeback_activity.java
@@ -1,14 +1,48 @@
 package kh.edu.rupp.ckcc.myproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class feeback_activity extends AppCompatActivity
-{
+public class feeback_activity extends AppCompatActivity implements View.OnClickListener {
+    private RadioGroup radioGroup;
+    private Button sendfeedback;
+    private EditText inputtext;
+    private RadioButton radioButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        radioGroup = findViewById(R.id.radiogroup);
+        inputtext = findViewById(R.id.txtfeedback_input);
+        sendfeedback = findViewById(R.id.btnsend_feedback);
+        sendfeedback.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        // get selected radio button from radioGroup
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+
+        // find the radiobutton by returned id
+        radioButton = (RadioButton) findViewById(selectedId);
+        String value = (String) radioButton.getText().toString();
+
+        String write = inputtext.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_SUBJECT, value);
+        intent.putExtra(Intent.EXTRA_TEXT, write);
+        // get this in description
+        intent.setType("Message/rfc822");
+        startActivity(Intent.createChooser(intent, "Choose app to send mail"));
+
+    }
+
 }
